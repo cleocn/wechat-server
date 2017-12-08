cc37ade995ffcca3749d06a40c55451e9d515fb6
ljx <cleocn@qq.com>
2017-12-08 15:12:39 +0800

    增加sentry

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-import micro from 'micro';
-
-import app from './app';
-import config from './config';
-
-micro(app).listen(config.port, () => {
-  console.log(`server listening ${config.port}`);
-});
